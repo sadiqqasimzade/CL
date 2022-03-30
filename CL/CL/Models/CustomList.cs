85fd35710b465b2920a682fb49f81208e924b194
sadiqqasimzade <tu6w5xs1c@code.edu.az>
2022-03-30 11:55:03 +0400

    first commit

---
+++ CL/CL/Models/CustomList.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CL.Models
+{
+    internal class CustomList<T>
+    {
+        private T[] _list = new T[1];
+        public int Count { get; private set; }
+        public int Capacity { get; }
+        public Type Typeof
+        {
+            get
+            { return typeof(T); }
+        }
+
+
+        public T this[int index]
+        {
+            get { return _list[index]; }
+            set { _list[index] = value; }
+        }
+        public CustomList(int capacity)
+        {
+            Capacity = capacity;
+        }
+        public CustomList()
+        {
+            Capacity = int.MaxValue;
+        }
+
+        public void Add(T input)
+        {
+        Point:
+            if (_list[_list.Length - 1] == null)
+            {
+                _list[Count] = input;
+                Count++;
+                Console.WriteLine("Added");
+            }
+            else
+            {
+                if (_list.Length * 2 > Capacity) throw new Exception("Capacity OverFlow");
+                Array.Resize(ref _list, _list.Length * 2);
+                Console.WriteLine($"Resized, {_list.Length} Length , {Count + 1} Current");
+                goto Point;
+            }
+        }
+        public void Reverse()
+        {
+            Array.Reverse(_list);
+            //T temp;
+            //for (int i = 0; i < _list.Length / 2; i++)
+            //{
+            //    temp = _list[i];
+            //    _list[i] = _list[_list.Length - i - 1];
+            //    _list[_list.Length - i - 1] = temp;
+            //}
+        }
+        public void Clear()
+        {
+            Array.Resize(ref _list, 2);
+            _list[0] = default;
+            _list[1] = default;
+            Count = 0;
+        }
+        public bool Exist(T value)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (_list[i].Equals(value))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Remove(T value)
+        {
+            int index = IndexOf(value);
+            RemoveAt(index);
+        }
+        public void RemoveAt(int index)
+        {
+            if (index! > _list.Length)
+            {
+                for (int i = index; i < _list.Length - 1; i++)
+                {
+                    _list[i] = _list[i + 1];
+                }
+                _list[_list.Length - 1] = default;
+            }
+        }
+
+        public int IndexOf(T value)
+        {
+            for (int i = 0; i < _list.Length - 1; i++)
+            {
+                if (_list[i].Equals(value))
+                    return i;
+            }
+            return -1;
+        }
+        public int LastIndexOf(T value)
+        {
+            for (int i = _list.Length - 1; i >= 0; i--)
+            {
+                if (_list[i].Equals(value)) return i;
+            }
+            return -1;
+        }
+        public void ShowInfo()
+        {
+            foreach (var item in _list)
+                
+                    Console.WriteLine(item);
+        }
+    }
+}
---
+++ CL/CL/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using CL.Models;
+namespace CL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte choise;
+            CustomList<int?> customList=new CustomList<int?>(10);
+            do
+            {
+            ChoisePoint:
+                try
+                {
+                    Console.Write("-------------\n1)Add\n2)Show\n3)Clear\n4)RemoveAt\n5)Remove\n6)Reverse\n7)exist\n8)Indexof\n9)LastIndex\nChoise:");
+                    choise = Convert.ToSByte(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    goto ChoisePoint;
+                }
+
+                switch (choise)
+                {
+                    case 0: break;
+                    case 1:
+                       
+                        try
+                        {
+                            customList.Add(3);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                            throw;
+                        }
+                        break;
+                        case 2:
+                        customList.ShowInfo();
+                        break;
+                    case 3:
+                        customList.Clear();
+                        break;
+                    case 4:
+                        customList.RemoveAt(2);
+                        break;
+                    case 5:
+                        customList.Remove(3);
+                        break;
+                    case 6:
+                        customList.Reverse();
+                        break;
+                    case 7:
+                        Console.WriteLine(customList.Exist(5));
+                        break;
+                    case 8:
+                        Console.WriteLine(customList.IndexOf(3));
+                        break;
+                    case 9:
+                        Console.WriteLine(customList.LastIndexOf(3));
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        break;
+                }
+            } while (choise != 0);
+
+
+        }
+    }
+}
